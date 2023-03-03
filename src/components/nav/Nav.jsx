693d1d3ae150f036f1238219d303edd5a6344f4b
Raptr12 <martinmesa1299@gmail.com>
2023-03-03 17:37:17 -0500

    Nav hook changes

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -5,15 +5,17 @@ import {FaUser} from 'react-icons/fa'
 import {FaBook} from 'react-icons/fa'
 import {MdDesignServices} from 'react-icons/md'
 import {RiMessage2Fill} from 'react-icons/ri'
+import { useState } from 'react'
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState ('#')
   return (
     <nav>
-        <a href="#"><AiFillHome/></a>
-        <a href="#about"><FaUser/></a>
-        <a href="#experience"><FaBook/></a>
-        <a href="#services"><MdDesignServices/></a>
-        <a href="#contact"><RiMessage2Fill/></a>
+        <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiFillHome/></a>
+        <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><FaUser/></a>
+        <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><FaBook/></a>
+        <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><MdDesignServices/></a>
+        <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><RiMessage2Fill/></a>
     </nav>
   )
 }
