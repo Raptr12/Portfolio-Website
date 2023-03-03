ef16d80c570145e6ae0273779b680515644359be
Raptr12 <martinmesa1299@gmail.com>
2023-03-03 17:06:08 -0500

    New Changes for the Nav Bar bavigation

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import './nav.css'
+import {AiFillHome} from 'react-icons/ai'
+import {FaUser} from 'react-icons/fa'
+import {FaBook} from 'react-icons/fa'
+import {MdDesignServices} from 'react-icons/md'
+import {RiMessage2Fill} from 'react-icons/ri'
 
 const Nav = () => {
   return (
     <nav>
-        <a href="#"></a>
+        <a href="#"><AiFillHome/></a>
+        <a href="#about"><FaUser/></a>
+        <a href="#experience"><FaBook/></a>
+        <a href="#services"><MdDesignServices/></a>
+        <a href="#contact"><RiMessage2Fill/></a>
     </nav>
   )
 }
