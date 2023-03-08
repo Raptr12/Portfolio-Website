58c951b245cfaf40050ce6f5aa50b89c85162506
Raptr12 <martinmesa1299@gmail.com>
2023-03-08 00:36:06 -0500

    Added Portfolio Button to Nav

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -4,6 +4,7 @@ import {AiFillHome} from 'react-icons/ai'
 import {FaUser} from 'react-icons/fa'
 import {FaBook} from 'react-icons/fa'
 import {MdDesignServices} from 'react-icons/md'
+import {BsStack} from 'react-icons/bs'
 import {RiMessage2Fill} from 'react-icons/ri'
 import { useState } from 'react'
 
@@ -15,6 +16,7 @@ const Nav = () => {
         <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><FaUser/></a>
         <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><FaBook/></a>
         <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><MdDesignServices/></a>
+        <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><BsStack/></a>
         <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><RiMessage2Fill/></a>
     </nav>
   )
